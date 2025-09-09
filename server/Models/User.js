687b5b0a3852dbae3b97314540cce9e32c286a5f
Luh Made Ayu Tasya Devi Pratishthita <luhmadeayutasasyadp@gmail.com>
2025-09-09 16:19:45 +0700

    feat: create User model

---
+++ server/Models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  googleId: {
+    type: String,
+    default: null,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", userSchema);
