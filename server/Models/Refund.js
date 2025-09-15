60b1dbf8861881f2239c25f373c1463de0b19a1d
Luh Made Ayu Tasya Devi Pratishthita <luhmadeayutasasyadp@gmail.com>
2025-09-15 15:31:33 +0700

    feat: create Refund Model

---
+++ server/Models/Refund.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const statusRefundSchema = new Schema(
+  {
+    status: {
+      type: String,
+      enum: ["REQUESTED", "APPROVED", "REJECTED", "PENDING", "FINISHED"],
+    },
+    changedBy: {
+      type: String,
+      enum: ["ADMIN", "SYSTEM"],
+    },
+  },
+  { timestamps: true, _id: false }
+);
+
+const refundSchema = new Schema(
+  {
+    userNameSnapshot: {
+      type: String,
+      required: true,
+    },
+    userNumberSnapshot: {
+      type: Number,
+      required: true,
+    },
+    //jangan lupa validasi isEmail di controller
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    refundMethod: {
+      type: String,
+      enum: ["ITEM_RETURN", "MONEY_RETURN"],
+      required: true,
+    },
+    midtransRefundId: {
+      type: String,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    adminNote: {
+      type: String,
+    },
+    status: {
+      type: [statusRefundSchema],
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      required: false,
+    },
+    deletedAt: {
+      type: Date,
+      required: fa,
+      lse,
+    },
+    refundItemId: {
+      type: [Schema.Types.ObjectId],
+      ref: "RefundItem",
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderId: {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Refund", refundSchema);
