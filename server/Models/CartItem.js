e3132292e001dd28d3dae0caf71cc02e1fd7fbaa
Luh Made Ayu Tasya Devi Pratishthita <luhmadeayutasasyadp@gmail.com>
2025-09-15 14:08:06 +0700

    feat(CartItem Model): insert reference field for every relation

--- server/Models/CartItem.js
+++ server/Models/CartItem.js
@@ -1,28 +1,33 @@
 import mongoose from "mongoose";
 
-const { Schema } = mongoose
+const { Schema } = mongoose;
 
-const cartItemSchema = new Schema ({
-    quantity:{
-        type: Number,
-        required: true
+const cartItemSchema = new Schema(
+  {
+    quantity: {
+      type: Number,
+      required: true,
     },
-    unitPriceSnapshot:{
-        type: Number,
-        required: true
+    unitPriceSnapshot: {
+      type: Number,
+      required: true,
     },
-    totalPrice:{
-        type: Number,
-        required: true
+    totalPrice: {
+      type: Number,
+      required: true,
     },
-    cartId:{
-        type: Schema.Types.ObjectId,
-        required: true
+    cartId: {
+      type: Schema.Types.ObjectId,
+      ref: "Cart",
+      required: true,
     },
-    productId:{
-        type: Schema.Types.ObjectId,
-        required: true
-    }
-}, {timestamps: false })
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: false }
+);
 
-export default mongoose.model("CartItem", cartItemSchema)
+export default mongoose.model("CartItem", cartItemSchema);
