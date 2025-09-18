586abbd960d49bae602e74e632004d26caf53a44
Luh Made Ayu Tasya Devi Pratishthita <luhmadeayutasasyadp@gmail.com>
2025-09-18 21:33:12 +0700

    feat (userController) = create delete user, register user, register admin

--- server/.router.js
+++ server/.router.js
@@ -3,10 +3,12 @@ import userController from "./_Controller/userController.js";
 const router = express.Router();
 
 //USER ROUTER
-router.post("/register", userController.register);
+router.post("/register", userController.registerCustomer);
+router.post("/register", userController.registerAdmin);
 router.post("/login", userController.login);
 router.get("/users", userController.getAllUsers);
 router.get("/user/:name", userController.getUserbyName);
 router.put("/user/:id", userController.updateUserbyUser);
+router.delete("/user/:id", userController.deleteUserbyId)
 
 export default router;
--- server/_Controller/userController.js
+++ server/_Controller/userController.js
@@ -3,10 +3,10 @@ import User from "../_Models/User.js";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
+
 const secret = process.env.JWT_SECRET;
 
-const register = async (req, res) => {
+const registerCustomer = async (req, res) => {
   try {
     const { name, phoneNumber, email, password, confirmPassword, role } = req.body;
 
@@ -82,6 +82,89 @@ const register = async (req, res) => {
       password: hashPassword,
       phoneNumber,
       role: "customer",
+    });
+    return res.status(200).json({
+      createUser,
+      message: "Register user successfull",
+    });
+  } catch (error) {
+    console.log("THIS IS ERROR>>>", error);
+    return res.status(500).json({
+      message: "Invalid server Error",
+    });
+  }
+};
+
+//===============//
+
+const registerAdmin = async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword, role } = req.body;
+
+    //VALIDASI
+
+    //EMPTY FORM
+    if (!email || !password || !confirmPassword || !name) {
+      return res.status(400).json({
+        message: "Fill all forms",
+      });
+    }
+
+    //CHECK VALID EMAIL
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        message: "Invalid email",
+      });
+    }
+
+    //CHECK PASSWORD LENGTH
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password should at least 6 character",
+      });
+    }
+
+    //PASS REGEX (MANUAL)
+    const regex = [/[a-z]/, /[A-Z]/, /\d/, /[@.#$!%^&*.?]/];
+    //.every() soa
+    const isStrong = regex.every((element) => element.test(password));
+    if (!isStrong) {
+      return res.status(400).json({
+        message: "Password is not strong enough",
+      });
+    }
+
+    //PASS REGEX (VALIDATOR)
+    if (!validator.isStrongPassword(password, { minLength: 6 })) {
+      return res.status(400).json({
+        message: "Password is not strong enough",
+      });
+    }
+
+    //CONFIRM PASSWORD
+    if (confirmPassword !== password) {
+      return res.status(400).json({
+        message: "Password do not match",
+      });
+    }
+
+    //CHECK USER EXIST
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({
+        message: "User with this email already exist",
+      });
+    }
+
+    //HASH PASSWORD
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //CREATE USER
+    const createUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+      role: "admin",
     });
     return res.status(200).json({
       createUser,
@@ -177,12 +260,14 @@ const getUserbyName = async (req, res) => {
   }
 };
 
+//===============//
+
 const updateUserbyUser = async (req, res) => {
   try {
     const { id } = req.params;
     const { name, phoneNumber, password, confirmPassword, email } = req.body;
-    console.log("ini PARAM ID", id)
-    console.log("ini yang dari login", req.user._id)
+    console.log("ini PARAM ID", id);
+    console.log("ini yang dari login", req.user._id);
     //VALIDASI YANG LOGIN SIAPA
     if (req.user._id !== id || req.user.role !== "admin") {
       return res.send(401).json({ message: "Not Authorized to change this user" });
@@ -202,10 +287,34 @@ const updateUserbyUser = async (req, res) => {
   }
 };
 
+//===============//
+
+const deleteUserbyId = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    if (req.user._id !== _id || req.user.role !== "admin") {
+      return res.send(401).json({ message: "Not Authorized to change this user" });
+    }
+    const deleteUser = await User.findByIdAndDelete({ id });
+    return res.status(200).json({
+      deleteUser,
+      message: "User deleted",
+    });
+  } catch (error) {
+    console.log("ERROR DI DELETE USER ==>", error);
+    return res.status(500).json({ message: "Invalid Server Error (DELETE USER)" });
+  }
+};
+
+//===============//
+
+
 export default {
-  register,
+  registerCustomer,
+  registerAdmin,
   login,
   getAllUsers,
   getUserbyName,
   updateUserbyUser,
+  deleteUserbyId,
 };
